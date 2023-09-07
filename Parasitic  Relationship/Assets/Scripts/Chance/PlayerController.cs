73ce35628db1a6ae95a0ebefea6b7f23479613f9
Chance Slaughter <chanceslaughter3@gmail.com>
2023-09-07 15:04:24 -0500

    scene setup
    setup a scene and some folders

---
+++ Parasitic  Relationship/Assets/Scripts/Chance/PlayerController.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.System;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 10;
+    public float jumpForce = 10;
+    public float mass = 1;
+    private rigidBody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
