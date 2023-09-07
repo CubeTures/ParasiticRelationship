6e8fd885776d09050e756c50514d3a48ca0de2c1
Chance Slaughter <chanceslaughter3@gmail.com>
2023-09-07 15:23:42 -0500

    Fixed compiler errors
    compiler errors were present from previous push, removed.

--- Parasitic  Relationship/Assets/Scripts/Chance/PlayerController.cs
+++ Parasitic  Relationship/Assets/Scripts/Chance/PlayerController.cs
@@ -1,18 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Unity.System;
 
 public class PlayerController : MonoBehaviour
 {
     public float speed = 10;
     public float jumpForce = 10;
     public float mass = 1;
-    private rigidBody2D rb;
+    private Rigidbody2D rb;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = this.GetComponent<Rigidbody2D>();
+        rb.mass = mass;
     }
 
     // Update is called once per frame
