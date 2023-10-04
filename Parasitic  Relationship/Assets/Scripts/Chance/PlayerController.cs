7ac159ea961ea61fad79fc839331fa4a351218b1
slaughter32 <51549987+slaughter32@users.noreply.github.com>
2023-10-04 13:42:21 -0500

    I changed some stuff to fix jumping and added features for jumping
    changed some things in level 0, was having problems with ground detection, so a ground layer has been created. Assign this to everything that the player should be able to actually touch.

--- Parasitic  Relationship/Assets/Scripts/Chance/PlayerController.cs
+++ Parasitic  Relationship/Assets/Scripts/Chance/PlayerController.cs
@@ -10,17 +10,36 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [Header("Settings: ")]
+
     [Tooltip("Remove layers from this mask to prevent them from interacting as ground to the player.\n(anything you dont want the player to be able to jump on)")]
     public LayerMask ground_layers;
     [Tooltip("This point shoots a ray horizontally (to the left) by x_size, if it hits the player is grounded")]
     public Transform grounded_ray;
+
+    [Header("Air Control")]
+
     [Tooltip("toggle the players ability to move in the air")]
     public bool air_control = false;
     [Tooltip("air control must be active.\n .9 = 90% control.\nSet to 1 to prevent any air control reduction.\nPositive values will work, but will make movement faster when in the air.")]
     public float reduce_air_control = .25f;
     [Tooltip("Toggles drag effects when in air. Applies to vertical too, can cause weird effect.")]
     public bool air_drag = false;
+
+    [Header("Jump Stuff")]
+
+    [Tooltip("How many jumps should the player have?")]
+    public int jumps = 1;
+    int jumpsLeft;
     float movementmod;
+    [Tooltip("Adjust the force applied when the player jumps")]
+    public float jumpForce = 500;
+    [Tooltip("Adjust the force applied on the second jump")]
+    public float doubleJumpForce = 10;
+    //variables to add
+    //public bool wallResetJump = false;
+
+    [Header("Movement")]
+
     [Tooltip("Adjust the players speed")]
     public float speed = 15;
     [Tooltip("Adjust the players mass")]
@@ -33,15 +52,10 @@ public class PlayerController : MonoBehaviour
     [Tooltip("Adjust movement dampening")]
     [Range(0f,100f)]
     public float dampenerStrength = 50;
-    [Tooltip("Adjust the force applied when the player jumps")]
-    public float jumpForce = 500;
-    [Tooltip("Adjust the force applied on the second jump")]
-    public float doubleJumpForce = 10;
+
+    [Header("Other")]
     public bool lock_rotation = true;
     [Header("Viewing Only")]
-    [Tooltip("here for debugging, changing it here will likely be overwritten in script")]
-    public bool doubleJump = false;
-    bool second_jump_used = false;
     public bool grounded = false;
     private Rigidbody2D rb;
     // Start is called before the first frame update
@@ -52,6 +66,7 @@ public class PlayerController : MonoBehaviour
         rb.angularDrag = angularDrag;
         rb.drag = linearDrag;
         rb.freezeRotation = lock_rotation;
+        jumpsLeft = jumps;
     }
 
     void Update(){
@@ -62,8 +77,13 @@ public class PlayerController : MonoBehaviour
     // FixedUpdate happens once every physics frame
     void FixedUpdate()
     {
+
         // || Physics2D.Raycast(new Vector2(this.transform.position.x-.5f, this.transform.position.y - .5f), Vector2.down, .1f)
         grounded = Physics2D.Raycast(grounded_ray.transform.position, new Vector2(-90, 0), x_size, ground_layers);
+        Debug.Log(grounded);
+        if (grounded && (jumpsLeft < jumps)) {
+            jumpsLeft = jumps;
+        }
         float x_force = rb.velocity.x * mass;
 
 
@@ -110,7 +130,8 @@ public class PlayerController : MonoBehaviour
 
     }
     void tryJump(){
-        if (grounded){
+        if (jumpsLeft > 0){
+            jumpsLeft--;
             rb.drag = linearDrag;
             rb.AddForce(new Vector2(0, jumpForce));
         }
